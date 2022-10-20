2c69317923e8218d6a16675874cb42376887e773
jharrisonio <james@jharrison.io>
2022-10-20 10:31:05 -0700

    Adding healthcheck route

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,37 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
+    if (!req.url) throw new Error('URL was undefined');
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    switch (url.pathname) {
+        case '/healthcheck':
+            healthcheck(req, res);
+            break;
+        default:
+            notFound(req, res)
+            break;
+    }
 });
+
+const healthcheck = (req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        status: 'OK',
+        errors: [],
+    }));
+}
+
+const notFound = (req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) => {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        status: 'ERROR',
+        errors: ['Requested resource not found'],
+    }));
+}
+
 
 server.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
